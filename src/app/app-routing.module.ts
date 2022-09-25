39ef51d6f31af3ad1649e373c98c9af13cd291c3
Naim Hakeem <nhakeem.arts@gmail.com>
2022-09-25 12:30:52 -0500

    hello world....again

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { CharactersComponent } from './characters/characters.component';
+import { HomeComponent } from './home/home.component';
+import { VolumesComponent } from './volumes/volumes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'characters', component: CharactersComponent},
+  { path: 'volumes', component: VolumesComponent},
+  { path: 'about', component: AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { VolumesComponent } from './volumes/volumes.component';
+import { CharactersComponent } from './characters/characters.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    VolumesComponent,
+    CharactersComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
