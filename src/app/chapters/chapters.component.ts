99360e4ceab3426de2a9f6b51983e1a919ef2086
Naim Hakeem <nhakeem.arts@gmail.com>
2024-07-01 22:14:17 -0500

    added video test

--- src/app/chapters/chapters.component.ts
+++ src/app/chapters/chapters.component.ts
@@ -12,8 +12,26 @@ export class ChaptersComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  
+  chapters = [
+    {
+        "chapterNumber": 1,
+        "title": "Pilot",
+        "description": "John Roach appears in the EntoVerse! How will he measure up to this world?"
+    },
+    {
+        "chapterNumber": 2,
+        "title": "Danger Ahead!",
+        "description": "With newfound trust of the locals, John Roach faces a threat head on!"
+    },
+    {
+        "chapterNumber": 3,
+        "title": "The Scarab King",
+        "description": "A new foe rallies against John Roach, can he thwart them?"
+    }
+  ];
 
-  showChapter(){
+  showChapter(chapterNumber: any){
     console.log("chapter is opening");
   }
 
