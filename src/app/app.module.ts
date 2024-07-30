f7bac245914ca7cde83a61a5f924d134ce9302a6
Naim Hakeem <nhakeem.arts@gmail.com>
2024-07-30 00:58:25 -0500

    latest angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,29 +8,22 @@ import { HomeComponent } from './home/home.component';
 import { ChaptersComponent } from './chapters/chapters.component';
 import { CharactersComponent } from './characters/characters.component';
 import { AboutComponent } from './about/about.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { ModalCharacterComponent } from './modal-character/modal-character.component';
 
-import { AngularFireModule } from '@angular/fire';
-import { AngularFireStorageModule } from '@angular/fire/storage';
+/*import { AngularFireModule } from '@angular/fire';
+import { AngularFireStorageModule } from '@angular/fire/storage';*/
 import { environment } from '../environments/environment';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    NavComponent,
-    HomeComponent,
-    ChaptersComponent,
-    CharactersComponent,
-    AboutComponent,
-    ModalCharacterComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
-  providers: [Title],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        NavComponent,
+        HomeComponent,
+        ChaptersComponent,
+        CharactersComponent,
+        AboutComponent,
+        ModalCharacterComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule], providers: [Title, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
