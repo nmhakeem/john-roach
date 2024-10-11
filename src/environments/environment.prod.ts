44d22dcedd85d837c3f3b59c765b44cafe603ef1
Naim Hakeem <nhakeem.arts@gmail.com>
2024-10-11 03:58:33 -0500

    added firebase to env.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyC0UnMpjS9YPHdlhtbr0shF8_7VzKw4Y1M",
+    authDomain: "john-roach.firebaseapp.com",
+    databaseURL: "https://john-roach-default-rtdb.firebaseio.com",
+    projectId: "john-roach",
+    storageBucket: "john-roach.appspot.com",
+    messagingSenderId: "473106392964",
+    appId: "1:473106392964:web:85b2b0cdc72d45dd7e1fd1",
+    measurementId: "G-155THXSX48"
+  }
 };
